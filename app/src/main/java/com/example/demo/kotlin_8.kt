826c920664e15da67d33b4acbe02ea3dab2d5166
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-09 18:40:24 +0900

    2020/04/09_5

--- app/src/main/java/com/example/demo/kotlin_8.kt
+++ app/src/main/java/com/example/demo/kotlin_8.kt
@@ -7,9 +7,7 @@ fun main(){
     val n = readLine()!!.toInt()
     val plans = Array(n + 1) {
         when (it) {
-            0 -> {
-                Plan(0, 0, 0)
-            }
+            0 -> Plan(0, 0, 0)
             else -> {
                 val (t, x, y) = readLine()!!.split(" ").map(String::toInt)
                 Plan(t, x, y)
@@ -19,9 +17,11 @@ fun main(){
     (0 until n).forEach { k ->
         val dt = plans[k + 1].t - plans[k].t
         val distance = abs(plans[k + 1].x - plans[k].x) + abs(plans[k + 1].y - plans[k].y)
-        if (distance > dt || distance % 2 != dt % 2) {
-            println("No")
-            return
+        when {
+            (distance <= dt && distance % 2 == dt % 2).not() -> {
+                println("No")
+                return
+            }
         }
     }
     println("Yes")
