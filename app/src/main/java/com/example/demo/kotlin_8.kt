df6cd9cbe62a0622e98c9c477262f483c1f0472d
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-09 18:14:20 +0900

    2020/04/09_4

--- app/src/main/java/com/example/demo/kotlin_8.kt
+++ app/src/main/java/com/example/demo/kotlin_8.kt
@@ -1,2 +1,28 @@
 package com.example.demo
 
+import kotlin.math.abs
+
+data class Plan(val t: Int, val x: Int, val y: Int)
+fun main(){
+    val n = readLine()!!.toInt()
+    val plans = Array(n + 1) {
+        when (it) {
+            0 -> {
+                Plan(0, 0, 0)
+            }
+            else -> {
+                val (t, x, y) = readLine()!!.split(" ").map(String::toInt)
+                Plan(t, x, y)
+            }
+        }
+    }
+    (0 until n).forEach { k ->
+        val dt = plans[k + 1].t - plans[k].t
+        val distance = abs(plans[k + 1].x - plans[k].x) + abs(plans[k + 1].y - plans[k].y)
+        if (distance > dt || distance % 2 != dt % 2) {
+            println("No")
+            return
+        }
+    }
+    println("Yes")
+}
