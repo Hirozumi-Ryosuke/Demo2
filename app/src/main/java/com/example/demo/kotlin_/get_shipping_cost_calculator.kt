bc8ac1977a105ca99f25acc4ab728149eccdd923
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-12 02:39:21 +0900

    2020/04/11_6

--- app/src/main/java/com/example/demo/kotlin_/get_shipping_cost_calculator.kt
+++ app/src/main/java/com/example/demo/kotlin_/get_shipping_cost_calculator.kt
@@ -5,19 +5,17 @@ fun main() {
 
     class Order(val itemCount: Int)
 
-    fun getShippingCostCalculator(
-        delivery: Delivery) : (Order) -> Double {
+    fun getShippingCostCalculator(delivery: Delivery): (Order) -> Double {
         when (delivery) {
             Delivery.EXPEDITED -> {
                 return { order -> 6 + 2.1 * order.itemCount }
             }
-            else -> return { order -> 1.2 * order.itemCount }
+            else -> {
+                return { order -> 1.2 * order.itemCount }
+            }
         }
-
     }
-
-    val calculator =
-        getShippingCostCalculator(Delivery.EXPEDITED)
+    val calculator = getShippingCostCalculator(Delivery.EXPEDITED)
     println("Shipping costs ${calculator(Order(3))}")
 
 }
