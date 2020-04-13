25a17ab2dd10cd8971751f9b6ca3113ff730d45d
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-13 23:24:58 +0900

    2020/04/13_5

--- app/src/main/java/com/example/demo/kotlin_/12.kt
+++ app/src/main/java/com/example/demo/kotlin_/12.kt
@@ -1,24 +1,11 @@
 package com.example.demo.kotlin_
 
-import android.os.Build.VERSION_CODES.*
-import androidx.annotation.RequiresApi
-import java.time.LocalDate
-
-@RequiresApi(O)
 fun main() {
-    operator fun ClosedRange<LocalDate>.iterator() =
-        object : Iterator<LocalDate> {
-            var current = start
-
-            override fun hasNext() =
-                current <= endInclusive
-
-            @RequiresApi(O)
-            override fun next() = current.apply {
-                current = plusDays(1)
-            }
+    fun printEntries(map: Map<String, String>) {
+        map.forEach { (key, value) ->
+            println("$key -> $value")
         }
-    val newYear = LocalDate.ofYearDay(2020, 1)
-    val daysOff = newYear.minusDays(1)..newYear
-    for (dayOff in daysOff) { println(dayOff) }
+    }
+    val map = mapOf("Oracle" to "Java", "JetBrains" to "Kotlin")
+    printEntries(map)
 }
