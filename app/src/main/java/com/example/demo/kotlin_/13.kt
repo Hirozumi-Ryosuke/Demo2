92ee612a090df96a6492d7fcf5cf50dd7fc82929
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-14 20:59:24 +0900

    2020/04/14_6

--- app/src/main/java/com/example/demo/kotlin_/13.kt
+++ app/src/main/java/com/example/demo/kotlin_/13.kt
@@ -4,12 +4,15 @@ fun main() {
     data class Person(val name: String, val age: Int? = null) {
 
         fun isOlderThan(other: Person): Boolean? {
-            if(age == null || other.age == null)
-                return null
-            return age > other.age
+            return when {
+                age == null || other.age == null -> null
+                else -> {
+                    age > other.age
+                }
+            }
         }
     }
-    println(Person("Sam", 35).isOlderThan(Person("Kay", 29)))
+    println(Person("Sam", 35).isOlderThan(Person("Kai", 29)))
     println(Person("Sam", 35).isOlderThan(Person("Amy", 42)))
     println(Person("Sam", 35).isOlderThan(Person("Jane")))
 }
