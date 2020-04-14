74d0d4d1b1cf9bf88a545f087b45fd572f21ebf5
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-15 00:17:59 +0900

    2020/04/14_7

---
+++ app/src/main/java/com/example/demo/kotlin_in_action/Expr.kt
@@ -0,0 +1,22 @@
+package com.example.demo.kotlin_in_action
+
+import java.lang.IllegalArgumentException
+
+interface Expr
+class Num(val value: Int): Expr
+class Sum(val left: Expr, val right: Expr): Expr
+
+fun main() {
+    fun eval(e: Expr): Int =
+        when (e) {
+            is Num ->
+                e.value
+            is Sum ->
+                eval(e.left) + eval(e.right)
+            else ->
+                throw IllegalArgumentException("Unknown expression")
+        }
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+}
+
+
