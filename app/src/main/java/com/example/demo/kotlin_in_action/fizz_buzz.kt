4b898bb9b48946a1bab93720678e551b2752bbb5
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-15 01:25:20 +0900

    2020/04/14_9

--- app/src/main/java/com/example/demo/kotlin_in_action/fizz_buzz.kt
+++ app/src/main/java/com/example/demo/kotlin_in_action/fizz_buzz.kt
@@ -7,7 +7,10 @@ fun main() {
         i % 5 == 0 -> "Buzz "
         else -> "$i "
     }
-    for (i in 1..100) {
+    (1..100).forEach { i ->
         print(fizzBuzz(i))
     }
+/*    (100 downTo 1 step 2).forEach { i ->
+        print(fizzBuzz(i))
+    }*/
 }
