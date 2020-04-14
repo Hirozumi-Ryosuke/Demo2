5731b937b30456dd737a66702a19dc24b3d2cf9a
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-15 02:56:22 +0900

    2020/04/14_finished

---
+++ app/src/main/java/com/example/demo/kotlin_in_action/tree_map.kt
@@ -0,0 +1,16 @@
+package com.example.demo.kotlin_in_action
+
+import java.util.*
+
+fun main() {
+    val binaryReps = TreeMap<Char, String>()
+
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryReps[c] = binary
+    }
+
+    for ((letter, binary) in binaryReps) {
+        println("$letter = $binary")
+    }
+}
