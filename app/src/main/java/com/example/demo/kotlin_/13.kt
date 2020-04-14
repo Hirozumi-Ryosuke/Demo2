e9f253e02930d1eb09e2415fc9a879ad9216be37
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-14 15:34:11 +0900

    2020/04/14_4

--- app/src/main/java/com/example/demo/kotlin_/13.kt
+++ app/src/main/java/com/example/demo/kotlin_/13.kt
@@ -1,7 +1,7 @@
 package com.example.demo.kotlin_
 
 fun main() {
-    val n = readLine()!!.toInt()
+    readLine()!!.toInt()
     val s = readLine()!!
     val fl = readLine()!!.split(" ").map { it.toInt() }
     fun f(c: Char) = '0' + fl[c - '1']
@@ -9,8 +9,8 @@ fun main() {
     val j = s.withIndex().indexOfFirst { (j, c) -> j > i && f(c) < c }.takeIf { it >= 0 } ?: s.length
     val ans =
         s.substring(0, i) +
-                s.substring(i, j).map { c -> f(c) }.joinToString("") +
-                        s.substring(j)
+        s.substring(i, j).map { c -> f(c) }.joinToString("") +
+        s.substring(j)
     println(ans)
 
 }
