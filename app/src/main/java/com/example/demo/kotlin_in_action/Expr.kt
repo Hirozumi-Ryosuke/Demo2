e68b4261669327a6a9bc4217336e958524a68543
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-15 00:43:29 +0900

    2020/04/14_8

--- app/src/main/java/com/example/demo/kotlin_in_action/Expr.kt
+++ app/src/main/java/com/example/demo/kotlin_in_action/Expr.kt
@@ -17,6 +17,23 @@ fun main() {
                 throw IllegalArgumentException("Unknown expression")
         }
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+
+    fun evalWithLogging(e: Expr): Int =
+        when (e) {
+            is Num -> {
+                println("num: ${e.value}")
+                e.value
+            }
+            is Sum -> {
+                val left = evalWithLogging(e.left)
+                val right = evalWithLogging(e.right)
+                println("Sum: $left + $right")
+                left + right
+            }
+            else -> throw IllegalArgumentException("Unknown expression")
+        }
+
+
 }
 
 
