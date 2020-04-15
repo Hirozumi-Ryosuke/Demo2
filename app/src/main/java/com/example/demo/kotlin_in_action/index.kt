adfd9a05821a1b9a67080e4ebe5ee0e8b537da63
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-15 11:48:07 +0900

    2020/04/15_1

---
+++ app/src/main/java/com/example/demo/kotlin_in_action/index.kt
@@ -0,0 +1,8 @@
+package com.example.demo.kotlin_in_action
+
+fun main() {
+    val list = arrayListOf("10", "11", "1001")
+    for ((index, element) in list.withIndex()) {
+        println("$index: $element")
+    }
+}
