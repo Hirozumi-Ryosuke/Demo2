7469b2c237fa31d209936695623b799e473e5d09
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-16 01:52:00 +0900

    2020/04/15_7

--- app/src/main/java/com/example/demo/in_action/s/chapter_3/set_Char_At.kt
+++ app/src/main/java/com/example/demo/in_action/s/chapter_3/set_Char_At.kt
@@ -2,14 +2,14 @@ package com.example.demo.in_action.s.chapter_3
 
 import java.lang.StringBuilder
 
-fun main() {
-    var StringBuilder.lastChar: Char
+var StringBuilder.lastChar: Char
     get() = get(length - 1)
-    set(value: Char) {
+    set(value) {
         this.setCharAt(length - 1, value)
     }
-    println("Kotlin".lastChar)
-    sb.lastChar = '!'
+
+fun main() {
     val sb = StringBuilder("Kotlin?")
+    sb.lastChar = '!'
     println(sb)
 }
