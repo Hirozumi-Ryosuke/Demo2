bf1b643be7acaf0f04fd1f048cd50a1685773da2
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-17 23:30:09 +0900

    2020/04/17_5

---
+++ app/src/main/java/com/example/demo/in_action/s/chapter_5/1.kt
@@ -0,0 +1,19 @@
+package com.example.demo.in_action.s.chapter_5
+
+data class Person(val name: String, val age: Int)
+
+fun main() {
+    fun findTheOldest(people: List<Person>) {
+        var maxAge = 0
+        var theOldest: Person? = null
+        for (person in people) {
+            if (person.age > maxAge) {
+                maxAge = person.age
+                theOldest = person
+            }
+        }
+        println(theOldest)
+    }
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+    findTheOldest(people)
+}
