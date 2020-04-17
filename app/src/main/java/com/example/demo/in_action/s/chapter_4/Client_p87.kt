25bfaf1c190d57f9f2246cc91138eb6ed054b9c8
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-17 12:56:27 +0900

    2020/04/17_2

--- app/src/main/java/com/example/demo/in_action/s/chapter_4/Client_p87.kt
+++ app/src/main/java/com/example/demo/in_action/s/chapter_4/Client_p87.kt
@@ -5,12 +5,23 @@ class Client(val name: String, private val postalCode: Int) {
     override fun toString() = "Client(name = $name, postalCode = $postalCode)"
 
     override fun equals(other: Any?): Boolean {
+        return when (other) {
+            null, !is Client -> false
+            else -> name == other.name && postalCode == other.postalCode
+        }
+    }
+/*
         if (other == null || other !is Client)
             return false
-        return name ==other.name &&
+        return name == other.name &&
                 postalCode == other.postalCode
     }
-
+*/
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + postalCode
+        return result
+    }
 }
 fun main() {
     val client1 = Client("Alice", 342562)
