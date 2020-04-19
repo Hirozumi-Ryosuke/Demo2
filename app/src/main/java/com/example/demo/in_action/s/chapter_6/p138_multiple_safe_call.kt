2cabeee68d8ac0846cc02724bf368a7d878de486
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-19 20:40:27 +0900

    2020/04/19_3

---
+++ app/src/main/java/com/example/demo/in_action/s/chapter_6/p138_multiple_safe_call.kt
@@ -0,0 +1,15 @@
+package com.example.demo.in_action.s.chapter_6
+
+class Address(val streetAddress: String, val zipCode: Int, val city: String, val country: String)
+
+class Company(val name: String, val address: Address?)
+
+class Person(val name: String, val company: Company?)
+
+fun main() {
+    fun Person.countryName(): String {
+        return company?.address?.country ?: "Unknown"
+    }
+    val person = Person("Dmitry", null)
+    println(person.countryName())
+}
