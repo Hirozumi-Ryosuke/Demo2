5cb567260598c17cffe60ea2e2f7a454c50b353e
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-22 01:33:58 +0900

    2020/04/21_2

---
+++ app/src/main/java/com/example/demo/in_action/s/chapter_6_2/p160_nullable_values.kt
@@ -0,0 +1,38 @@
+package com.example.demo.in_action.s.chapter_6_2
+
+import java.io.BufferedReader
+import java.io.StringReader
+
+fun main() {
+    fun readNumbers(reader: BufferedReader): List<Int?> {
+        val result = ArrayList<Int?>()
+        for (line in reader.lineSequence()) {
+            try {
+                val number = line.toInt()
+                result.add(number)
+            } catch (e: NumberFormatException) {
+                result.add(null)
+            }
+        }
+        return result
+    }
+
+    fun addValidNumbers(numbers: List<Int?>) {
+        var sumOfValidNumbers = 0
+        var invalidNumbers = 0
+        for (number in numbers) {
+            if (number != null) {
+                sumOfValidNumbers += number
+            } else {
+                invalidNumbers++
+            }
+        }
+        println("Sum of valid numbers: $sumOfValidNumbers")
+        println("Invalid numbers: $invalidNumbers")
+    }
+
+    val reader = BufferedReader(StringReader("1\nabc\n42"))
+    val numbers =readNumbers(reader)
+    addValidNumbers(numbers)
+
+}
