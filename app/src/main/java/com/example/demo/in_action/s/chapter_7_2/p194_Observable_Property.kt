74072049ebebcb053c516f4765367b497ea96324
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-24 17:28:30 +0900

    2020/04/24_1

---
+++ app/src/main/java/com/example/demo/in_action/s/chapter_7_2/p194_Observable_Property.kt
@@ -0,0 +1,36 @@
+package com.example.demo.in_action.s.chapter_7_2
+
+import java.beans.PropertyChangeListener
+import java.beans.PropertyChangeSupport
+import kotlin.reflect.KProperty
+
+
+class ObservableProperty(
+    var propValue: Int, val changeSupport: PropertyChangeSupport
+) {
+    operator fun getValue(p: Person, prop: KProperty<*>): Int = propValue
+
+    operator fun setValue(p: Person, prop: KProperty<*>, newValue: Int) {
+        val oldValue = propValue
+        propValue = newValue
+        changeSupport.firePropertyChange(prop.name, oldValue, newValue)
+    }
+}
+class Person(val name: String, age: Int, salary: Int): PropertyChangeAware() {
+    var age: Int by ObservableProperty(age, changeSupport)
+    val salaly: Int by ObservableProperty(salary, changeSupport)
+}
+
+fun main() {
+    val p = Person("Dmitry", 34, 2000)
+    p.addPropertyChangeListener(
+        PropertyChangeListener { event ->
+            println(
+                "Property ${event.propertyName} changed " +
+                        "from ${event.oldValue} to ${event.newValue}"
+            )
+        }
+    )
+    p.age = 35
+    p.salary = 2100
+}
