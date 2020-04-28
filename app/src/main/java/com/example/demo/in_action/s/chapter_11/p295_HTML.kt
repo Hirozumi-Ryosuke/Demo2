c9800ed74ebb5a21cd796a146e7de2c013974c9b
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-04-29 03:39:57 +0900

    2020/04/28_1

---
+++ app/src/main/java/com/example/demo/in_action/s/chapter_11/p295_HTML.kt
@@ -0,0 +1,37 @@
+package com.example.demo.in_action.s.chapter_11
+
+import kotlin.math.ceil
+
+open class Tag(val name: String) {
+    private val children = mutableListOf<Tag>()
+
+    protected fun <T : Tag> doInit(child: T, init: T.() -> Unit) {
+        child.init()
+        children.add(child)
+    }
+
+    override fun toString() =
+        "<$name>${children.joinToString("")}</$name>"
+}
+
+fun table(init: TABLE.() -> Unit) = TABLE().apply(init)
+
+class TABLE : Tag("table") {
+    fun tr(init: TR.() -> Unit) = doInit(TR(), init)
+}
+class TR : Tag("tr") {
+    fun td(init: TD.() -> Unit) = doInit(TD(), init)
+}
+class TD : Tag("td")
+
+fun createTable() =
+    table {
+        tr {
+            td {
+            }
+        }
+    }
+
+fun main() {
+    println(createTable())
+}
